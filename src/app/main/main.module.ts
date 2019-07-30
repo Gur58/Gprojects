7907d57207e8a599a5278559c1ac18eb098f2abb
Gur58 <evg8774@yandex.ru>
2019-07-30 21:17:27 +0300

    add footer

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { MainComponent }   from './main.component';
 import { HeaderComponent } from '../header/header.component';
+import {FooterComponent} from "../footer/footer.component";
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ MainComponent,  HeaderComponent],
+    declarations: [ MainComponent,  HeaderComponent, FooterComponent],
     bootstrap:    [ MainComponent ]
 })
 export class MainModule { }
